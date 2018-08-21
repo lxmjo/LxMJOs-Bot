41da614523cf70094568dfa10dbe98cb1e520269
lxmjo <42571964+lxmjo@users.noreply.github.com>
2018-08-21 12:47:01 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,7 @@
+const Discord = require("discord.js");
+const YTDL = require("ytdl-core");
+
+const TOKEN = "NDgxMTU3NDgyODcwMzQxNjMy.DlzZYg.0yy4HJgDc9HhNZbfFLcCzHT3QHo";
+const PREFIX = ".";
+
+bot.login(TOKEN);
